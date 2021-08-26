25f294ebe7cffde850a376a153373c4a9ed146dd
Martinfbr <m.felipe59@hotmail.com>
2021-08-25 21:45:28 -0500

    Correcion de errores

--- Consulta.js
+++ Consulta.js
@@ -1,28 +1,20 @@
-const mysql = require ('mysql')
+const mysql = require('mysql')
 
 const conexion = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'' ,
-    database :'prueba'
+    password:'',
+    database:'prueba'
 })
-
 conexion.connect((error)=>{
     if(error) throw error 
-    console.log('Conexion Exitosa!')
+    console.log('conexion exitorsa')
 })
 
-
-
-conexion.query('SELECT * FROM persona', (error, rows)=>{
-    if(error) throw error
-    console.log('Consulta de la informacion de la tabla')
+//Consultas
+conexion.query('SELECT*FROM persona',(error, rows)=>{
+    if(error) throw error 
+    console.log('COnsulta de la informacion')
     console.log(rows)
 })
-conexion.query('SELECT * FROM producto', (error, rows)=>{
-    if(error) throw error
-    console.log('Consulta de la informacion de la tabla')
-    console.log(rows)
-})
-
 conexion.end()
