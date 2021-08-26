0bdc05361903df1a6e95515a3ff1315094332c2d
Carlos Cañizares <cecanizares23@gmail.com>
2021-08-26 11:47:27 -0500

    Jason and Express

--- Consulta.js
+++ Consulta.js
@@ -1,20 +1,38 @@
 const mysql = require('mysql')
+const express = require('express')
+const app = express()
 
 const conexion = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'',
-    database:'prueba'
+    database:'sakila'
 })
 conexion.connect((error)=>{
     if(error) throw error 
     console.log('conexion exitorsa')
 })
 
-//Consultas
-conexion.query('SELECT*FROM persona',(error, rows)=>{
-    if(error) throw error 
-    console.log('COnsulta de la informacion')
-    console.log(rows)
+app.get('/', function(req,resp){
+    conexion.query('SELECT * FROM actor', function (error, rows){
+        if(!!error){
+            console.log('conexion fallida')
+        }
+        else{
+            console.log('consulta de la informacion de la tabla')
+            console.log(rows)
+            conexion.end()
+        }
+    })
+
+
 })
-conexion.end()
+app.listen(3000)
+
+// Consultas
+// conexion.query('SELECT*FROM persona',(error, rows)=>{
+//     if(error) throw error 
+//     console.log('COnsulta de la informacion')
+//     console.log(rows)
+// })
+// conexion.end()
